7a7a9e84ca20de753a272a580dfb20c0e7312a1c
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-20 12:45:11 +0530

    changed font and added header

--- inspiration_app/lib/main.dart
+++ inspiration_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,22 +16,51 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'Raleway',
       ),
-      home: const MyHomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(child: Text("Hello")),
+      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        elevation: 0,
+        leading: IconButton(icon: Icon(Icons.menu, color: Colors.black87,), onPressed: (() {
+     
+        }),),
       ),
+      body: SafeArea(child: Column(
+        children: [
+          Container(
+            width: double.infinity, /*full width*/
+            decoration: BoxDecoration(color: Colors.white),
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Find Your", style: TextStyle(color: Colors.black87, fontSize: 25),),
+                SizedBox(height: 3.0,),
+                Text("Inspiration", style: TextStyle(color: Colors.black, fontSize: 40),),
+              ],
+            ),
+          ),
+        ],
+      ),)
     );
   }
 }
