6ef718af5f0da72912accc851f856b788a004939
Dushani-Ranasinghe <uththaradushani@gmail.com>
2023-02-20 14:16:06 +0530

    created inspiration app basic ui

---
+++ inspiration_app/lib/assets/components/promo_card.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+class PromoCard extends StatelessWidget {
+  const PromoCard({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  final image;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.62 / 3,
+      child: Container(
+        margin: EdgeInsets.only(right: 15.0),
+        decoration: BoxDecoration(
+          color: Colors.orange,
+          borderRadius: BorderRadius.circular(12.0),
+          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.0),
+              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+                0.1,
+                0.9
+              ], colors: [
+                Colors.black.withOpacity(0.8),
+                Colors.black.withOpacity(0.1),
+              ])),
+        ),
+      ),
+    );
+  }
+}
--- inspiration_app/lib/main.dart
+++ inspiration_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'assets/components/promo_card.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,13 +18,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily: 'Raleway',
+        fontFamily: 'Roboto',
       ),
       home: const HomePage(),
     );
   }
 }
-
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -32,36 +33,143 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(244, 243, 243, 1),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        systemOverlayStyle: SystemUiOverlayStyle.light,
-        elevation: 0,
-        leading: IconButton(icon: Icon(Icons.menu, color: Colors.black87,), onPressed: (() {
-     
-        }),),
-      ),
-      body: SafeArea(child: Column(
-        children: [
-          Container(
-            width: double.infinity, /*full width*/
-            decoration: BoxDecoration(color: Colors.white),
-            padding: EdgeInsets.all(20.0),
+        backgroundColor: Color.fromRGBO(244, 243, 243, 1),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black87,
+            ),
+            onPressed: (() {}),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: SafeArea(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Find Your", style: TextStyle(color: Colors.black87, fontSize: 25),),
-                SizedBox(height: 3.0,),
-                Text("Inspiration", style: TextStyle(color: Colors.black, fontSize: 40),),
+                Container(
+                  width: double.infinity,
+                  /*full width*/
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(bottom: Radius.circular(30))),
+                  padding: EdgeInsets.only(top: 3, bottom: 5, left: 20, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Find Your",
+                        style: TextStyle(color: Colors.black87, fontSize: 25),
+                      ),
+                      SizedBox(
+                        height: 3.0,
+                      ),
+                      Text(
+                        "Inspiration",
+                        style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5.0),
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(244, 243, 243, 1),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              prefixIcon: Icon(
+                                Icons.search,
+                                color: Colors.black87,
+                              ),
+                              hintText: "Search you're looking for",
+                              hintStyle: TextStyle(
+                                  color: Colors.grey, fontSize: 19.0)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15.0,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Promo Today",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w900),
+                      ),
+                      SizedBox(height: 15.0),
+                      SizedBox(
+                        height: 200,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            PromoCard(image: 'lib/assets/images/one.jpg'),
+                            PromoCard(image: 'lib/assets/images/two.jpg'),
+                            PromoCard(image: 'lib/assets/images/three.jpg'),
+                            PromoCard(image: 'lib/assets/images/four.jpg'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.0),
+                          image: DecorationImage(
+                              fit: BoxFit.cover, image: AssetImage("lib/assets/images/five.jpg")),
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12.0),
+                              gradient: LinearGradient(
+                                  begin: Alignment.bottomRight,
+                                  stops: [
+                                    0.3,
+                                    0.9
+                                  ],
+                                  colors: [
+                                    Colors.black.withOpacity(0.8),
+                                    Colors.black.withOpacity(0.2),
+                                  ])),
+                                  child: Align(
+                                    alignment: Alignment.bottomLeft,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(15.0),
+                                      child: Text("Best Design", style: TextStyle(color: Colors.white, fontSize: 20),),
+                                    ),
+                                  ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
-        ],
-      ),)
-    );
+        ));
   }
 }
-
